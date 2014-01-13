b4e195b8b36d61a2aab50480534b7d4f57ad62c5
David Mazieres <dm@uun.org>
2014-01-12 17:07:07 -0800

    more fd stuff

--- fdstream.cc
+++ fdstream.cc
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <sys/socket.h>
 
 #include "fdstream.h"
 #include "muchsync.h"
@@ -12,6 +13,37 @@
 using namespace std;
 
 const string shell = "/bin/sh";
+
+int
+cmd_iofd (const string &cmd)
+{
+  int fds[2];
+  if (socketpair(AF_UNIX, SOCK_STREAM, 0, fds) < 0)
+    throw runtime_error (string ("socketpair: ") + strerror (errno));
+
+  pid_t pid = fork();
+  switch (pid) {
+  case -1:
+    throw runtime_error (string ("fork: ") + strerror (errno));
+    break;
+  case 0:
+    close (fds[0]);
+    if (fds[1] != 0)
+      dup2 (fds[1], 0);
+    if (fds[1] != 1)
+      dup2 (fds[1], 1);
+    if (fds[1] != 0 && fds[1] != 1)
+      close (fds[1]);
+    execl (shell.c_str(), shell.c_str(), "-c", cmd.c_str(), nullptr);
+    cerr << shell << ": " << strerror (errno) << '\n';
+    _exit (1);
+    break;
+  default:
+    close (fds[1]);
+    fcntl (fds[0], F_SETFD, 1);
+    return fds[0];
+  }
+}
 
 string
 cmd_output (const string &cmd)
